3d5ce4d587acb21d2198eadf48b476f48826bb05
李佐群 <2645929971@qq.com>
2024-12-27 15:58:25 +0800

    build(deps): 添加 unplugin-auto-import 插件
    - 在 package.json 中添加 unplugin-auto-import依赖
    - 在 vite.config.ts 中配置 AutoImport 插件
    - 更新 tsconfig.json 和 tsconfig.node.json 以支持新功能
    - 新增 auto-import.d.ts 文件，用于自动引入 Vue 相关 API

--- src/views/sendMessage/index.vue
+++ src/views/sendMessage/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-
 onMounted(() => {
   const information = document.getElementById('info') as HTMLDivElement;
   information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    // 自动引入
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts'
+    })],
   resolve: {
     alias: {
       //别名配置
